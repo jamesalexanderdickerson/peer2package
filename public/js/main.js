2f1be43359faad074d2f79a8c32055fecf72204c
jamesalexanderdickerson <jamesalexanderdickerson@gmail.com>
2016-08-09 01:13:18 -0400

    integrated vanilla javascript into angular

--- public/js/main.js
+++ public/js/main.js
@@ -16,12 +16,41 @@
   peer2package.controller('mainController', function($scope) {});
 
   peer2package.controller('menuController', function($scope) {
-    $scope.authStatus = false;
-    return console.log(regForm);
+    return $scope.authStatus = false;
   });
 
   peer2package.controller('mapController', function($scope) {});
 
   peer2package.controller('aboutController', function($scope) {});
+
+  peer2package.directive('menuChange', function() {
+    return {
+      restrict: 'AE',
+      link: function() {
+        var arrow, btn_about, btn_home, menu, sidenavmenu;
+        menu = document.getElementById('menu');
+        sidenavmenu = document.getElementById('side-nav-menu');
+        arrow = document.getElementById('arrow');
+        btn_home = document.getElementById('home');
+        btn_about = document.getElementById('about');
+        menu.addEventListener('click', function() {
+          menu.classList.toggle('open');
+          return sidenavmenu.classList.toggle('nav-open');
+        });
+        btn_about.addEventListener('click', function() {
+          arrow.classList.add('about');
+          arrow.classList.remove('home');
+          btn_about.classList.add('active');
+          return btn_home.classList.remove('active');
+        });
+        return btn_home.addEventListener('click', function() {
+          arrow.classList.add('home');
+          arrow.classList.remove('about');
+          btn_home.classList.add('active');
+          return btn_about.classList.remove('active');
+        });
+      }
+    };
+  });
 
 }).call(this);
--- src/js/main.coffee
+++ src/js/main.coffee
@@ -9,7 +9,34 @@ peer2package.controller 'mainController', ($scope) ->
 
 peer2package.controller 'menuController', ($scope) ->
   $scope.authStatus = false
-  console.log regForm
+
 peer2package.controller 'mapController', ($scope) ->
 
 peer2package.controller 'aboutController', ($scope) ->
+
+peer2package.directive 'menuChange', () ->
+  return {
+    restrict: 'AE',
+    link: () ->
+      menu = document.getElementById 'menu'
+      sidenavmenu = document.getElementById 'side-nav-menu'
+      arrow = document.getElementById 'arrow'
+      btn_home = document.getElementById 'home'
+      btn_about = document.getElementById 'about'
+
+      menu.addEventListener 'click', () ->
+        menu.classList.toggle 'open'
+        sidenavmenu.classList.toggle('nav-open')
+
+      btn_about.addEventListener 'click', () ->
+        arrow.classList.add 'about'
+        arrow.classList.remove 'home'
+        btn_about.classList.add 'active'
+        btn_home.classList.remove 'active'
+
+      btn_home.addEventListener 'click', () ->
+        arrow.classList.add 'home'
+        arrow.classList.remove 'about'
+        btn_home.classList.add 'active'
+        btn_about.classList.remove 'active'
+  }
